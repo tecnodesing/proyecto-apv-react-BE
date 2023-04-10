e50ee1f91738e9024176835438135d9415d49402
tecnodesing <snors21@gmail.com>
2023-04-09 22:10:27 -0600

    se configuro variable de entorno

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 dotenv.config();
 conectarDB();
 
-const dominiosPermitidos =['http://192.168.0.108:5173']
+const dominiosPermitidos =[process.env.FRONTEND_URL]
 
 const corsOptions = {
     origin: function(origin, callback){
